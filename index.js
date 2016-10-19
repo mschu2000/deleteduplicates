f2bf8ff99ba0a19ed6b08a7980f12bd0b07cd261
Schumacher Martin <SCHUMACM@power.inet>
2016-10-19 08:09:10 +0200

    ES2015 Promise all und es geht!

--- index.js
+++ index.js
@@ -25,35 +25,32 @@ var options = {
 
 
 
-let lese_verzeichnis = () => function() {
-    return new Promise((resolve) => {
-        glob(config.filefilter, options)
-            .then(function(files) {
-                // files is an array of filenames.
-                // If the `nonull` option is set, and nothing
-                // was found, then files is ["**/*.js"]
-                // er is an error object or null.
-                console.log("Anzahl der Files = " + files.length);
-                for (var i in files) {
-                    console.log(files[i]);
-                }
-                resolve(files.length);
-            });
-    });
-};
+let lese_verzeichnis = new Promise((resolve) => {
+    glob(config.filefilter, options)
+        .then(function(files) {
+            // files is an array of filenames.
+            // If the `nonull` option is set, and nothing
+            // was found, then files is ["**/*.js"]
+            // er is an error object or null.
+            console.log("Anzahl der Files = " + files.length);
+            for (var i in files) {
+                console.log(files[i]);
+            }
+            resolve(files.length);
+        });
+});
 
-let myWait3Sec = () => function() {
-    return Promise((resolve) => {
-        // Set a timeout for 3 seconds
-        setTimeout(function() {
-            // It's been 3 seconds, alert via callback
-            console.log('3 Sek sind um!');
-            resolve(3);
-        }, 3000);
-    });
-};
+let myWait3Sec = new Promise((resolve) => {
+    // Set a timeout for 3 seconds
+    setTimeout(function() {
+        // It's been 3 seconds, alert via callback
+        console.log('3 Sek sind um!');
+        resolve(3);
+    }, 3000);
+});
 
-Promise.all([lese_verzeichnis(), myWait3Sec()]).then(values => {
+
+Promise.all([lese_verzeichnis, myWait3Sec]).then(values => {
     console.log(values); // [3, 1337, "foo"]
     //let [anz_files, anz_sekunden] = values;
     let anz_files = values[0];
