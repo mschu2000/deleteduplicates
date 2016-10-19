362c6d07db443a53b72825a0e773a19f6143353c
Schumacher Martin <SCHUMACM@power.inet>
2016-10-19 08:04:34 +0200

    ES2015 Promise, aber liefert Funktionen statt Werte

--- index.js
+++ index.js
@@ -23,35 +23,40 @@ var options = {
     "cwd": config.dir.candidates
 };
 
-async function lese_verzeichnis() {
-    glob(config.filefilter, options)
-        .then(function(files) {
-            // files is an array of filenames.
-            // If the `nonull` option is set, and nothing
-            // was found, then files is ["**/*.js"]
-            // er is an error object or null.
-            console.log("Anzahl der Files = " + files.length);
-            for (var i in files) {
-                console.log(files[i]);
-            }
-            return files.length;
-        });
-}
 
-async function myWait3Sec() {
-    // Set a timeout for 3 seconds
-    setTimeout(function() {
-        // It's been 3 seconds, alert via callback
-        console.log('3 Sek sind um!');
-        return 3;
-    }, 3000);
-}
 
-let [anz_files, anz_sekunden] = await Promise.all(
-    [
-        lese_verzeichnis(),
-        myWait3Sec()
-    ]
-);
+let lese_verzeichnis = () => function() {
+    return new Promise((resolve) => {
+        glob(config.filefilter, options)
+            .then(function(files) {
+                // files is an array of filenames.
+                // If the `nonull` option is set, and nothing
+                // was found, then files is ["**/*.js"]
+                // er is an error object or null.
+                console.log("Anzahl der Files = " + files.length);
+                for (var i in files) {
+                    console.log(files[i]);
+                }
+                resolve(files.length);
+            });
+    });
+};
 
-console.log(`alles fertig: anz_files = ${anz_files} gelesen in ${anz_sekunden} Sekunden`);
+let myWait3Sec = () => function() {
+    return Promise((resolve) => {
+        // Set a timeout for 3 seconds
+        setTimeout(function() {
+            // It's been 3 seconds, alert via callback
+            console.log('3 Sek sind um!');
+            resolve(3);
+        }, 3000);
+    });
+};
+
+Promise.all([lese_verzeichnis(), myWait3Sec()]).then(values => {
+    console.log(values); // [3, 1337, "foo"]
+    //let [anz_files, anz_sekunden] = values;
+    let anz_files = values[0];
+    let anz_sekunden = values[1];
+    console.log(`alles fertig: anz_files = ${anz_files} gelesen in ${anz_sekunden} Sekunden`);
+});
